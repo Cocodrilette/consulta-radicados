8208c10f6eeecc1f0349dc15afdd0f5cc856bb85
cocodrilette <cocodrilette@gmail.com>
2025-06-18 19:37:17 -0500

    Add query_file_number action to process selected snapshots and update ProcessAdmin to display last_update

--- webscraper/files_consultant/actions/process.py
+++ webscraper/files_consultant/actions/process.py
@@ -1,3 +1,4 @@
+from django.db.models.query import QuerySet
 from django.contrib import messages
 from django.utils.timezone import datetime
 
@@ -5,9 +6,7 @@ from files_consultant import models as files_consultant_models
 from files_consultant.utils import rama_judicial as rama_judicial_utils
 
 
-def query_file_number(
-    _, request, queryset: list[files_consultant_models.ProcessSnapshot]
-):
+def query_file_number(_, request, queryset: QuerySet[files_consultant_models.Process]):
     """
     Admin action to process selected snapshots.
     """
@@ -16,41 +15,39 @@ def query_file_number(
         return
 
     processed_snapshots = []
-    for snapshot in queryset:
-        data_list = rama_judicial_utils.get_radicado_data(
-            [snapshot.process.file_number]
-        )
+    for process_obj in queryset:
+        data_list = rama_judicial_utils.get_radicado_data([process_obj.file_number])
 
         if data_list and len(data_list) > 0:
             radicado_data = data_list[0]
 
-            snapshot.process.open_date = datetime.strptime(
+            process_obj.open_date = datetime.strptime(
                 radicado_data.open_date, "%Y-%m-%d"
             ).date()
 
             parties = rama_judicial_utils.extract_legal_parties(
                 radicado_data.legal_parties_str
             )
-            snapshot.process.plaintiff = (
+            process_obj.plaintiff = (
                 parties.plaintiff.title() if parties.plaintiff else None
             )
-            snapshot.process.defendant = (
+            process_obj.defendant = (
                 parties.defendant.title() if parties.defendant else None
             )
-            snapshot.process.procurator = (
+            process_obj.procurator = (
                 parties.procurator.title() if parties.procurator else None
             )
 
-            snapshot.save()
+            process_obj.save()
 
             files_consultant_models.ProcessSnapshot.objects.create(
-                process=snapshot.process,
+                process=process_obj,
                 last_update=datetime.strptime(
                     f"{radicado_data.last_update_date}", "%Y-%m-%d"
                 ),
             )
 
-            processed_snapshots.append(snapshot.process.file_number)
+            processed_snapshots.append(process_obj.file_number)
 
     if processed_snapshots:
         messages.success(
--- webscraper/files_consultant/admin.py
+++ webscraper/files_consultant/admin.py
@@ -1,18 +1,47 @@
 from django.contrib import admin
 
 from files_consultant import models as files_consultant_models
-from files_consultant.actions import process_snapshot as process_snapshot_actions
+from files_consultant.actions import process as process_actions
 
 
 @admin.register(files_consultant_models.Process)
 class ProcessAdmin(admin.ModelAdmin):
-    list_display = ("file_number", "open_date", "plaintiff", "defendant", "procurator")
+    list_display = (
+        "file_number",
+        "last_update",
+        "open_date",
+        "plaintiff",
+        "defendant",
+        "procurator",
+    )
     search_fields = ("file_number", "plaintiff", "defendant", "procurator")
     list_filter = ("open_date",)
     fieldsets = (
         (None, {"fields": ("file_number", "open_date")}),
         ("Parties", {"fields": ("plaintiff", "defendant", "procurator")}),
     )
+
+    def last_update(self, obj):
+        return (
+            obj.processsnapshot_set.last().last_update
+            if obj.processsnapshot_set.exists()
+            else None
+        )
+
+    @admin.action(description="Consultar Radicado")
+    def query_file_number(self, request, queryset):
+        """
+        Admin action to process selected snapshots.
+        """
+        try:
+            process_actions.query_file_number(self, request, queryset)
+        except Exception as e:
+            self.message_user(
+                request, f"Error processing snapshots: {str(e)}", level="error"
+            )
+            return
+
+    actions = [query_file_number]
 
 
 @admin.register(files_consultant_models.ProcessSnapshot)
@@ -21,18 +50,3 @@ class ProcessSnapshotAdmin(admin.ModelAdmin):
     search_fields = ("process__file_number",)
     list_filter = ("snapshot_date", "last_update")
     fieldsets = ((None, {"fields": ("process", "last_update")}),)
-
-    @admin.action(description="Consultar Radicado")
-    def query_file_number(self, request, queryset):
-        """
-        Admin action to process selected snapshots.
-        """
-        try:
-            process_snapshot_actions.query_file_number(self, request, queryset)
-        except Exception as e:
-            self.message_user(
-                request, f"Error processing snapshots: {str(e)}", level="error"
-            )
-            return
-
-    actions = [query_file_number]
